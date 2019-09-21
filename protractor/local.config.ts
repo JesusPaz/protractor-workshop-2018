7e213f1c3088b660c8c38a4526a476f1809aeac3
Jesus Paz <jeherpabo@gmail.com>
2019-09-21 10:34:06 -0500

    changes to selenium server

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,12 +1,27 @@
-import { Config, browser  } from 'protractor';
+import { browser, Config } from 'protractor';
 import { reporter } from './helpers/reporter';
 
-export const config: Config = {
+export let config: Config = {
   framework: 'jasmine',
-  specs: [ '../test/google.spec.js' ],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['../test/**/*.spec.js'],
+  SELENIUM_PROMISE_MANAGER: false,
+  noGlobals: true,
+  getPageTimeout: 30000,
+  capabilities: {
+    browserName: 'chrome',
+    shardTestFiles: true,
+    maxInstances: 2,
+    chromeOptions: {
+      args: ['disable-infobars=true', '--window-size=800,600', '–no-sandbox', '–disable-dev-shm-usage', '-headless'],
+      prefs: { credentials_enable_service: false }
+    }
+  },
+  jasmineNodeOpts: {
+    defaultTimeoutInterval: 120000
+  },
   onPrepare: () => {
-    browser.ignoreSynchronization = true;
     reporter();
+    browser.ignoreSynchronization = true;
+    browser.manage().timeouts().implicitlyWait(0);
   }
 };
